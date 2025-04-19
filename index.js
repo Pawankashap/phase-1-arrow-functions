25494d294e7a797a4f514522efb429ed768d173b
Pawankashap <126440752+Pawankashap@users.noreply.github.com>
2025-04-19 01:56:18 -0700

    add string arrow fun

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ const divide=(v1=2000,v2=100)=> v1/v2;
 const nums = [1,2,3];
 const square =(v1=2,v2=2)=> v1*v2;
 const add=(v1=5,v2=20)=> v1+v2; 
+const showname=(fname='Pawan',lname='Kashyap')=> `Welcome ${fname} ${lname}`
