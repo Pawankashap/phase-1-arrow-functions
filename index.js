f0b29c951268d2644a50301656a2c8801ac473c5
Pawankashap <126440752+Pawankashap@users.noreply.github.com>
2025-04-19 02:01:55 -0700

    add multi line in arrow fun

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const divide=(v1=2000,v2=100)=> v1/v2;
 const nums = [1,2,3];
 const square =(v1=2,v2=2)=> v1*v2;
 const add=(v1=5,v2=20)=> v1+v2; 
-const showname=(fname='Pawan',lname='Kashyap')=> `Welcome ${fname} ${lname}`
+const showname=(fname='Pawan',lname='Kashyap')=> {
+   console.log(`Welcome ${fname} ${lname}`);
+    console.log(`How are you Mr. ${fname} ${lname}`);
+    return 'Thank you'
+}
